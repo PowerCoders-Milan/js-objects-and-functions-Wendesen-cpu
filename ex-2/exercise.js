158ac3586bf8f4c2096ad20a1aee0781a0c391b4
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-26 15:50:26 +0100

    Done

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,26 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+window.alert("Here we go!");
+var input = window.prompt("If you want to know about your future, then input the word PLAY");
+
+
+
+function fortuneTeller(){
+
+    var numChild = [0,1,2,3,4,5,6,7,8,9];
+    var partnersName=["Matha","Jessica","Betty","Alessia","Alem","Tirhas","Chaltu","Semira","Melika","Meseret"];
+    var jobTitle =["Engineer","Doctor","Programmer","Professor","Footballer","Trader","politician","Pastor","Singer","Manager"];
+    var location = ["USA", "Italy", "UK", "Japan", "Africa", "Ethiopia", "Cameroon", "England", "Austalia", "China"];
+   
+    var randonNum = Math.floor(Math.random() * 10);
+
+    window.alert(`In the future, you will get married to ${partnersName[randonNum]} and will have ${numChild[randonNum]} kids. You will also be a successful ${jobTitle[randonNum]} and will be living in ${location[randonNum]}.`) 
+
+}
+
+if (input == "PLAY"){
+    fortuneTeller();
+    
+}
