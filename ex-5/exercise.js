9d5e5ac2c6981557567c34fea9f2cad2861b4b93
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-26 16:25:04 +0100

    Done

---
+++ ex-5/exercise.js
@@ -0,0 +1,30 @@
+
+var name = window.prompt("Insert your name");
+var lan = window.prompt("Select your language please, Insert 1 for Amharic, 2 for Spanish , 3 for Oromic, 4 for Chinese, anythin else foe English");
+
+if(lan == 1){
+    window.alert(`ሰላም ለአንተ ${name}`);
+}
+
+else if(lan == 2){
+
+    window.alert(`cómo estás ${name}`);
+
+     }
+
+else if(lan == 3){
+
+    window.alert(`Akkam Jirta ${name}`);
+
+    }
+
+else if(lan == 4){
+
+    window.alert(`你好吗 ${name}`);
+
+  }
+
+else {
+    window.alert(`How are you ${name}`);
+  }
+
