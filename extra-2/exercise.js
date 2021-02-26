d09d8cbf960dba4ee1b9078d1e90adb8c091b1fe
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-26 17:57:47 +0100

    Done but not confirmed yet.I have to check again later.

---
+++ extra-2/exercise.js
@@ -0,0 +1,20 @@
+var nome = window.prompt("Give me you name");
+var birthD = window.prompt("Insert the date you were born");
+var occup = window.prompt("What do you work. I mean what is your occupattion?");
+var cty = window.prompt("which city you live in?");
+
+function objectGenerator(a,b,c,d,){
+
+
+        name = a;
+        birthDate = b;
+        occupation = c;
+        city = d;
+
+
+
+}
+
+objectGenerator(nome,birthD,occup,cty);
+
+window.alert(objectGenerator.city);
