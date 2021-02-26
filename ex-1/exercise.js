5f1c74be22dd4b83136fd6c87d618cea8073a927
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-26 15:43:21 +0100

    done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,10 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+var city = window.prompt("Enter the name of the capital city");
+var country = window.prompt("Enter the country");
+var second = window.prompt("Enter the second biggest city of that country");
+
+window.alert(`${city} is the capital city of ${country} and ${second} is the second biggest city`);
+
