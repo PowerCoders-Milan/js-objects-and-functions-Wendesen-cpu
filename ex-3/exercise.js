d217e75202da65899db662615b855cf7674a267d
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-26 15:59:02 +0100

    Done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,8 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+var choice = ["orange", "Obama", "Football"];
+window.alert(`My first choice of color is ${choice[0]}`);
+window.alert(`My first choice of president is ${choice[1]}`);
+window.alert(`My first choice of sport is ${choice[2]}`);
